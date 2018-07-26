00bc812889b4198c55197d83fae95a43f9300afd
always2390 <always2390@gmail.com>
2018-07-26 13:43:39 -0500

    KotlinTest

---
+++ app/src/main/java/com/example/always2390/kotlintest/KotlinTestMainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.always2390.kotlintest
+
+import android.app.Activity
+import android.os.Bundle
+
+class KotlinTestMainActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_kotlin_test_main)
+    }
+}
