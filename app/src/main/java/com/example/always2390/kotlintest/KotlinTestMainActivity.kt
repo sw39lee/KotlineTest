9df266201116343a9d873d90282085858e0ccc68
always2390 <always2390@gmail.com>
2018-07-26 14:50:03 -0500

    creating new button by Kotlin

--- app/src/main/java/com/example/always2390/kotlintest/KotlinTestMainActivity.kt
+++ app/src/main/java/com/example/always2390/kotlintest/KotlinTestMainActivity.kt
@@ -1,12 +1,25 @@
 package com.example.always2390.kotlintest
 
 import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 
 class KotlinTestMainActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_kotlin_test_main)
+
+        findViewById<TextView>(R.id.main_text).setText("Text")
+        val bt = findViewById<Button>(R.id.main_button) as Button
+        bt.setText("Button")
+        bt.setOnClickListener {
+            startActivity(Intent(this, NormalJavaActivity::class.java))
+        }
+
     }
+
 }
---
+++ app/src/main/java/com/example/always2390/kotlintest/NormalJavaActivity.java
@@ -0,0 +1,14 @@
+package com.example.always2390.kotlintest;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class NormalJavaActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_normal_java);
+    }
+
+}
